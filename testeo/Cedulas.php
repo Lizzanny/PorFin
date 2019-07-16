f00c7dd7bf043e31416444bfd56c64bdc5d01980
Alex10161010 <eliasalejandromome@gmail.com>
2019-07-16 10:37:23 -0500

    hereda la clase tester

--- testeo/Cedulas.php
+++ testeo/Cedulas.php
@@ -7,15 +7,71 @@
     esto con el fin de comprobar la conexion y generar un reporte
 */
 include_once '../Libs/ConexionOracle.php'; 
+include_once 'teste.php';
 class Cedulas extends ClaseTesteo{
 
-    private $cnxcedula;
+  
+    public function getconexionremota(){
+        //echo $this->cnx[0]['host']; 
+        $co = oci_connect($this->cnx[0]['user'], $this->cnx[0]['pass'], "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = ".$this->cnx[0]['host']." )(PORT = 1521)) (CONNECT_DATA =  (SID =".$this->cnx[0]['base'].")))");
+            // Seleccion de la base de datos 
+            if(!$co){
+                $error = oci_error();
+                trigger_error(htmlentities($error['message'], ENT_QUOTES), E_USER_ERROR);
+               //echo 'u.u   xdxxx';
+            }else{
+                $this->getInformacionCedulas($co); 
+               //echo "conexion exitosa de php a oracle <br> xxsss";
+            }
+    }
 
-    public function  obtnerInformacionCedulas(){
-    $this->cnxcedula = mysqli_connect($this->host,$this->user,$this->pass)or die ("Error en la conexion a la BBDD"); 
-        // Seleccion de la base de datos 
-        mysqli_select_db($this->cnxcedula,$this->daba)or die("No se a encontrado la base de datos");
-        mysqli_set_charset($this->conM, "utf8");
+    public function getInformacionCedulas($co){
+        $inf=array(); 
+        $sql="SELECT
+        CUENTAS.CCDES CUENTA,
+        CEDULAS.CONTCT CENTRO_TRABAJO,
+        CEDULAS.CONTNUM CEDULA,
+        GetNumActivoxCed(CEDULAS.CONTCT,CEDULAS.CONTNUM),
+        CEDULAS.CONTCC,
+        CEDULAS.CONTSC,
+        CEDULAS.CONTSSC,
+        CEDULAS.CONTSSSC,
+        CEDULAS.CONTDES,
+        CEDULAS.CONTFECHADQ,
+        CEDULAS.CONTFACTURA,
+        CEDULAS.CONTCOSTO,
+        CEDULAS.CONTORIGBIEN,
+        CEDULAS.CONTASADEP,
+        CEDULAS.CONTFECHCAP,
+        CEDULAS.CONTFECHDEP,
+        CEDULAS.CONTPOLIZA,
+        CEDULAS.CONTREFALTAS,
+        CEDULAS.CONTREFBAJAS,
+        CEDULAS.CONTABONO,
+        CEDULAS.CONTFECHMOV,
+        CEDULAS.CONTDEPMEN,
+        CEDULAS.CONTDEPANUAL,
+        CEDULAS.CONTDEPACUM,
+        CEDULAS.CONTSALXDEP,
+        CEDULAS.CONTBAJADEP,
+        CEDULAS.CONTMESDEP,
+        CEDULAS.CONTFECHDETDEP,
+        CEDULAS.CONTFECHBAJA
+        FROM CEDULAS,CUENTAS
+        WHERE CEDULAS.CONTCC=CUENTAS.CCNUM AND CEDULAS.CONTFECHMOV <= TO_DATE('31122018','DDMMYYYY')
+        ORDER BY 2,3"; 
+
+        $stmt = oci_parse($co, $sql);
+        oci_execute($stmt);
+
+        for ($i=0; $row = oci_fetch_array($stmt, OCI_BOTH); $i++){
+            $inf=array('cuenta'=> $row['CUENTA'],
+                        'centro' => $row['CENTRO_TRABAJO']);
+        }
+        oci_free_statement($stmt);
+        oci_close($co);
+
+        echo json_encode($inf);
     }
     
 
@@ -24,6 +80,6 @@ class Cedulas extends ClaseTesteo{
 }
 
 $ob = new Cedulas();
-$ob->getMetodoAcceso();  
+$ob->getconexionremota();  
 $ob->cerrarConexion2(); 
 ?>
--- testeo/teste.php
+++ testeo/teste.php
@@ -10,10 +10,11 @@ include_once '../Libs/ConexionOracle.php';
 class ClaseTesteo extends ConexionOracle
 {
     //atributo 
-    private $cnx= array(); //informacion de conexion
+    protected $cnx= array(); //informacion de conexion
 
     function __construct(){
-		parent::__construct();
+        parent::__construct();
+        $this->getMetodoAcceso();
 	}
 
     //metodo para obtener la informacion de las bases de datos remotas
@@ -64,86 +65,18 @@ class ClaseTesteo extends ConexionOracle
         return $checar; 
     }
 
-    public function getconexionremota(){
-        //echo $this->cnx[0]['host']; 
-        $co = oci_connect($this->cnx[0]['user'], $this->cnx[0]['pass'], "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = ".$this->cnx[0]['host']." )(PORT = 1521)) (CONNECT_DATA =  (SID =".$this->cnx[0]['base'].")))");
-            // Seleccion de la base de datos 
-            if(!$co){
-                $error = oci_error();
-                trigger_error(htmlentities($error['message'], ENT_QUOTES), E_USER_ERROR);
-               echo 'u.u   xdxxx';
-            }else{
-                $this->getInformacionCedulas($co); 
-               echo "conexion exitosa de php a oracle <br> xxsss";
-            }
-    }
-
-    public function getInformacionCedulas($co){
-        $inf=array(); 
-        $sql="SELECT
-        CUENTAS.CCDES CUENTA,
-        CEDULAS.CONTCT CENTRO_TRABAJO,
-        CEDULAS.CONTNUM CEDULA,
-        GetNumActivoxCed(CEDULAS.CONTCT,CEDULAS.CONTNUM),
-        CEDULAS.CONTCC,
-        CEDULAS.CONTSC,
-        CEDULAS.CONTSSC,
-        CEDULAS.CONTSSSC,
-        CEDULAS.CONTDES,
-        CEDULAS.CONTFECHADQ,
-        CEDULAS.CONTFACTURA,
-        CEDULAS.CONTCOSTO,
-        CEDULAS.CONTORIGBIEN,
-        CEDULAS.CONTASADEP,
-        CEDULAS.CONTFECHCAP,
-        CEDULAS.CONTFECHDEP,
-        CEDULAS.CONTPOLIZA,
-        CEDULAS.CONTREFALTAS,
-        CEDULAS.CONTREFBAJAS,
-        CEDULAS.CONTABONO,
-        CEDULAS.CONTFECHMOV,
-        CEDULAS.CONTDEPMEN,
-        CEDULAS.CONTDEPANUAL,
-        CEDULAS.CONTDEPACUM,
-        CEDULAS.CONTSALXDEP,
-        CEDULAS.CONTBAJADEP,
-        CEDULAS.CONTMESDEP,
-        CEDULAS.CONTFECHDETDEP,
-        CEDULAS.CONTFECHBAJA
-        FROM CEDULAS,CUENTAS
-        WHERE CEDULAS.CONTCC=CUENTAS.CCNUM AND CEDULAS.CONTFECHMOV <= TO_DATE('31122018','DDMMYYYY')
-        ORDER BY 2,3"; 
-
-        $stmt = oci_parse($co, $sql);
-        oci_execute($stmt);
-
-        for ($i=0; $row = oci_fetch_array($stmt, OCI_BOTH); $i++){
-            $inf=array('cuenta'=> $row['CUENTA'],
-                        'centro' => $row['CENTRO_TRABAJO']);
-        }
-        oci_free_statement($stmt);
-        oci_close($co);
-
-        echo json_encode($inf);
-    }
-
-
-
-
-
-
 
 
     //metodo publico de acceso para llamar a metodo privados
     public function getMetodoAcceso(){
         $this->getDatosConexionesRemotas(); 
-        $this->getconexionremota();
+       
         //echo json_encode($this->cnx); 
     }
 
 }//endmyClass
 
     $ob = new ClaseTesteo();
-    $ob->getMetodoAcceso();  
+    //$ob->getMetodoAcceso();  
     $ob->cerrarConexion2(); 
 ?>
